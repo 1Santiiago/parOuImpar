c1ebee469087ff828713a1cc48d2b558f4b664ae
1Santiiago <83790888+1Santiiago@users.noreply.github.com>
2022-03-03 18:57:21 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+//Par ou impar?
+//requisistos->:
+//receber uma quantidade de valores para avaliar
+//função exibe se cada valor é impar ou par
+
+function exibirTipo(e) {
+  for (let i = 0; i <= e; i++)
+    if (i % 2 === 0) {
+      // console.log(i, 'Par')
+    } else {
+      // console.log(i, 'Impar')
+    }
+}
